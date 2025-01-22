b64b386ace0f883e71e8a0966dac5b93076def48
shreesriv12 <shreeyasrivastava1124@gmail.com>
2025-01-23 00:56:20 +0530

    SECOND PAGE

--- frontend/src/pages/FirstPage.jsx
+++ frontend/src/pages/FirstPage.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const FirstPage = () => {
+  const [active1, setActive1] = useState(false);
+
+  var handleClick1 = () => {
+    setActive1(!active1); 
+  };
+  const [active2, setActive2] = useState(false);
+
+   const handleClick2 = () => {
+    setActive2(!active2); 
+  };
+  const [active3, setActive3] = useState(false);
+
+  const  handleClick3 = () => {
+    setActive3(!active3); 
+  };
+  const [active4, setActive4] = useState(false);
+
+  const  handleClick4 = () => {
+    setActive4(!active4); 
+  };
+  const [active5, setActive5] = useState(false);
+
+  const  handleClick5 = () => {
+    setActive5(!active5); 
+  };
+  const [active6, setActive6] = useState(false);
+
+  const  handleClick6 = () => {
+    setActive6(!active6); 
+  };
+  const [active7, setActive7] = useState(false);
+
+  const  handleClick7 = () => {
+    setActive7(!active7); 
+  };
+  const [active8, setActive8] = useState(false);
+
+  const  handleClick8 = () => {
+    setActive8(!active8); 
+  };
+  const [active9, setActive9] = useState(false);
+
+  const  handleClick9 = () => {
+    setActive9(!active9); 
+  };
   return (
     <div className="bg-white h-screen">
       <nav className="p-4 fixed top-0 left-0 w-full flex justify-between items-center shadow-md">
@@ -37,19 +83,35 @@ const FirstPage = () => {
 
       <div className="flex space-x-20 mt-20 ">
         <div className="w-80 h-80 style={{
-    boxShadow: '10px 10px 30px rgba(181, 183, 169, 0.75)' // Red shadow with some transparency
+    boxShadow: '10px 10px 30px rgba(181, 183, 169, 0.75)' 
   }} bg-gradient-to-br from-blue-500 via-purple-500 to-pink-600 text-white rounded flex items-center justify-center rounded-lg">
         </div>
         <div
   className="w-80 h-80 bg-gray-100 text-black border-4 border-black rounded-lg flex items-center justify-center">
     <img src='/e-cell logo.png'/>
     </div>
-
-        <div className="w-80 h-80 bg-grey-500 text-white rounded flex items-center justify-center rounded-lg">
+        <div className="w-80 h-80 bg-gray-100 border-4 border-gray-500 text-white rounded-lg flex flex-wrap items-center justify-center"
+  style={{
+    boxShadow: 'inset 0 0 1rem rgb(157, 163, 176)'
+  }}>
+     <div className="flex flex-wrap gap-6 mt-2 mb-2">
+      <div className={`w-20 h-20  ${active1?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick1}></div>
+      <div className={`w-20 h-20  ${active2?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick2}></div>
+      <div className={`w-20 h-20  ${active3?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick3}></div>
+      </div>
+      <div className="flex flex-wrap gap-6 mt-2 mb-2">
+      <div className={`w-20 h-20  ${active4?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick4}></div>
+      <div className={`w-20 h-20  ${active5?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick5}></div>
+      <div className={`w-20 h-20  ${active6?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick6}></div>
+      </div>
+      <div className="flex flex-wrap gap-6 mt-2 mb-2">
+      <div className={`w-20 h-20  ${active7?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick7}></div>
+      <div className={`w-20 h-20  ${active8?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick8}></div>
+      <div className={`w-20 h-20  ${active9?'bg-blue-900':'bg-red-900'} border border-gray-300 rounded-xl`} onClick={handleClick9}></div>
+      </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- frontend/src/pages/SecondPage.jsx
+++ frontend/src/pages/SecondPage.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import "./aboutme.css"
+import { Link } from 'react-router-dom'
 const SecondPage = () => {
   return (
-    <div>
-      hello
+       <div className="portfolio-container">
+        <div className="left-container">
+            <div className="content">
+                <p className="intro"><span>Shreeya Srivastava</span></p>
+                <h1>20243267</h1>
+                <p className ="detail">I am a first year undergraduate from MNNIT Allahabad <br/> persuing Btech. I am a CP and Web Deveplopment <br></br> enthusiast and looking forward for more <br></br>opportunities in future to explore my capabilities in the best way<br></br> possible </p>
+                <div className="btn-container">
+                <button>  <Link to="https://www.instagram.com/shreesriv12/">Instagram</Link></button>
+               <button> <Link to="https://www.linkedin.com/in/shreeya-srivastava-94a458327/"> Linkdln</Link></button>
+                </div>
+            </div>
+        </div>
+        <div className="right-container"></div>
+        <div className="design">
+            <div className="circle"></div>
+            <div className="circle"></div>
+        </div>
     </div>
   )
 }
